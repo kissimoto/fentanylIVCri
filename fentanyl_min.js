f4d0a7377bc5a88a26c8190c5835e35dbdc2ca0c
kissimoto <bmc_hit@yahoo.com>
2020-05-10 20:43:14 +0100

    Update fentanyl_min.js

--- fentanyl_min.js
+++ fentanyl_min.js
@@ -1,1 +1,38 @@
-function fentanylIVprocess(){const e=document.getElementById("weight").value,o=document.getElementById("dose_rate").value,t=document.getElementById("plannedFluidRate").value,n=document.getElementById("bagSize"),l=t*e;console.log(l);const c=o/t;console.log(c);const d=n.value;console.log(d);const u=c*d;console.log(u);const s=u/.05;console.log(s);const g=s/1e3,m=(Math.round(100*g)/100).toFixed(2);document.getElementById("result").innerHTML="Fluid rate per hour = "+(Math.round(100*l)/100).toFixed(2)+" ml/hr <br>Drug volume = "+m+" ml."}
+
+function fentanylIVprocess(){
+    const weight = document.getElementById('weight').value
+    const dose_rate = document.getElementById('dose_rate').value
+    const plannedFluidRate = document.getElementById('plannedFluidRate').value
+    const bagSize = document.getElementById('bagSize')
+ 
+    // 1. Calculate fluid administration rate by multiplying
+    // 'plannedFluidRate' by 'weight'.
+ 
+    const pfr = plannedFluidRate * weight
+    console.log(pfr)
+
+    // 2. Medikamentenberechnung = 'dose_rate' geteilt durch 'pfr'
+   
+    const drugMlFluid = dose_rate / plannedFluidRate
+    console.log (drugMlFluid)
+
+    // 3. Totaldosierung mit dem Medikament Fentanyl
+     
+    const ivBagSize = bagSize.value
+    console.log(ivBagSize)
+
+    const totalDrug = drugMlFluid * ivBagSize
+    console.log(totalDrug)
+
+    // 4. Gesamtberechnung des Medikamentes 'drugVolume'
+
+    const drugVolume = totalDrug / 0.05
+    console.log(drugVolume)
+
+    const drugVolumeInMicrogram = drugVolume / 1000
+
+    const drugVolInMicroRound = (Math.round(drugVolumeInMicrogram * 100) / 100).toFixed(2)
+   
+    document.getElementById('result').innerHTML = "Fluid rate per hour = " + (Math.round(pfr * 100) / 100).toFixed(2) + " ml/hr <br>Drug volume = "  
+    + drugVolInMicroRound + " ml." 
+}
